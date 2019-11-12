974a39ba481f25e81ab9e4f04736ea3ab835e08a
Jim Heeresma <jheeresm@f0r3s12.codam.nl>
2019-11-12 15:08:01 +0100

    push_swap.h

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -14,18 +14,60 @@
 # define PUSH_SWAP_H
 
 # include "../c_libraries/libft/includes/libft.h"
-# include "../c_libraries/libsort/includes/libsort.h"
 
-#define PART_SIZE 5
+# define PART_SIZE 5
+# define M(x) ft_modulo(x, data.a->len + data.b->len)
+
+typedef struct		s_array
+{
+	int				*stack;
+	int				len;
+}					t_array;
+
+typedef struct		s_flags
+{
+	char			v;
+}					t_flags;
+
+typedef struct		s_solution
+{
+	char			*solution;
+}					t_solution;
 
 typedef struct		s_data
 {
 	t_array			*a;
 	t_array			*b;
 	char			*solution;
+	t_flags			flags;
+	t_solution		*sol;
 }					t_data;
 
-t_data				*parse_input(t_data *data, int argc, char **argv);
-void				add_operation(t_data *data, char *op);
+t_data				*parse_input(t_data data, int argc, char **argv);
+void				add_operation(t_data data, char *op);
+t_array				*new_array(int len);
+void				free_t_array(t_array *a);
+t_array				*get_longest_subseq(t_data data, t_array arr);
+void				rotations(t_data data, int a_rot, int b_rot, int rot_both);
+void				rev_rotations(t_data data, int a_rot, int b_rot,
+																int rot_both);
+void				display(t_data data);
+void				display_help(void);
+void				solver(t_data data);
+
+/*
+** operations.
+*/
+void				pa(t_data data, int bool);
+void				pb(t_data data, int bool);
+void				ra(t_data data, int bool);
+void				rb(t_data data, int bool);
+void				rr(t_data data, int bool);
+void				rra(t_data data, int bool);
+void				rrb(t_data data, int bool);
+void				rrr(t_data data, int bool);
+void				sa(t_data data, int bool);
+void				sb(t_data data, int bool);
+void				ss(t_data data, int bool);
 
 #endif
---
+++ shared/array_help.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   array_help.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jheeresm <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/12 14:13:39 by jheeresm      #+#    #+#                 */
+/*   Updated: 2019/11/12 14:13:39 by jheeresm      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+void	free_t_array(t_array *a)
+{
+	if (!a)
+		return ;
+	free(a->stack);
+	free(a);
+}
+
+t_array	*new_array(int len)
+{
+	t_array *new;
+
+	if (len <= 0)
+		ft_error("ERROR: new_array() called with len <= 0");
+	new = malloc(sizeof(t_array));
+	if (!new)
+		ft_error("ERROR: malloc failed (new_array)");
+	new->stack = malloc(sizeof(int) * len);
+	if (!new->stack)
+		ft_error("ERROR: malloc failed (new_array)");
+	new->len = len;
+	return (new);
+}
+
+int		get_pos(t_array *a, int x)
+{
+	int i;
+	int *stack;
+	int len;
+
+	if (!a)
+		ft_error("ERROR: get_pos() called without array");
+	stack = a->stack;
+	len = a->len;
+	i = 0;
+	while (i < len / 2)
+	{
+		if (stack[i] == x)
+			return (i);
+		if (stack[-i] == x)
+			return (-i);
+		i++;
+	}
+	ft_error("ERROR: get_pos() called but int not in array.");
+	return (0);
+}
